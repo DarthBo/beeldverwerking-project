ab6ce29115e1e57ee00ff4d5c3d8a284fe63ab3b
Tibault Damman <tibault.damman@gmail.com>
2014-04-05 21:53:51 +0200

    Use const char* instead of passing string copies and converting to char* afterwards.

--- src/file_utils.h
+++ src/file_utils.h
@@ -2,10 +2,10 @@
 #define FILE_UTILS_H
 
 // Returns true if file exists
-bool exists(const std::string& name)
+bool exists(const char* name)
 {
     struct stat buffer;
-    return (stat (name.c_str(), &buffer) == 0);
+    return (stat (name, &buffer) == 0);
 }
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,8 @@
 #include <cstdio>
+#include <sys/stat.h>
 #include "opencv2/opencv.hpp"
 #include "opencv2/core/core.hpp"
 #include "opencv2/highgui/highgui.hpp"
-#include <sys/stat.h>
 #include "image_utils.h"
 #include "file_utils.h"
 #include "video_utils.h"
@@ -13,30 +13,38 @@ using cv::imshow;
 using cv::waitKey;
 using cv::destroyWindow;
 
-const std::string videoLocation = "../resources/vid/20140226_h_10fps.avi";
-const std::string windowName1 = "Image stuffs";
-const std::string windowName2 = "Processed image stuffs";
-const std::string windowName3 = "More Processed image stuffs";
+const char* videoLocation = "../resources/vid/20140226_h_10fps.avi";
+const char* windowName1 = "Image stuffs";
+const char* windowName2 = "Processed image stuffs";
+const char* windowName3 = "More Processed image stuffs";
 
 
 int main(int argc, char **argv)
 {
-    std::cout<<(exists(videoLocation) == true ? "FILE FOUND" : "FILE NOT FOUND")<<std::endl;
+    std::cout << (exists(videoLocation) == true ? "FILE FOUND" : "FILE NOT FOUND") << std::endl;
+
     namedWindow(windowName1);
     namedWindow(windowName2);
     namedWindow(windowName3);
+
     Mat frame;
     getFrameByNumber(videoLocation,1,frame);
+
     Mat green;
     filterColour(frame,GREEN_MIN,GREEN_MAX,green);
+
     Mat filtered;
     textureFilter(frame,filtered);
-    imshow( windowName1.c_str(), frame);
-    imshow( windowName2.c_str(), green);
-    imshow( windowName3.c_str(), filtered);
+
+    imshow(windowName1, frame);
+    imshow(windowName2, green);
+    imshow(windowName3, filtered);
+
     waitKey();
+
     destroyWindow(windowName1);
     destroyWindow(windowName2);
     destroyWindow(windowName3);
+
     return 0;
 }
