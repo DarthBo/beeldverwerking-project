d7215e037dd739ecf6a3df387089346519c816e9
hannesdm <hannes.dm@hotmail.com>
2014-04-05 22:53:44 +0200

    move window over frame and call feature functions
    time to go to work SVM team

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,11 @@ using cv::Mat;
 using cv::imshow;
 using cv::waitKey;
 using cv::destroyWindow;
+using cv::Rect;
+using std::string;
+using std::vector;
+using std::cout;
+using std::endl;
 
 const std::string videoLocation = "../resources/vid/20140226_h_10fps.avi";
 const std::string windowName1 = "Image stuffs";
@@ -22,6 +27,40 @@ const std::string windowName3 = "More Processed image stuffs";
 int main(int argc, char **argv)
 {
     std::cout<<(exists(videoLocation) == true ? "FILE FOUND" : "FILE NOT FOUND")<<std::endl;
+
+    Mat frame;
+    getFrameByNumber(videoLocation,1,frame);
+    Rect window = Rect(0, 0, 16, 9);
+    cout<< frame.cols <<" "<<frame.rows<<endl;
+    for(int row = 0; row< frame.rows; row+= window.height ){
+        for(int col = 0; col < frame.cols;col += window.width){
+            Mat ROI(frame,window); // region of interest
+            double greenFeature = getAverageColour(ROI,GREEN_MIN,GREEN_MAX);
+            vector<double> textureFeatures;
+            getAverageTexture(ROI,textureFeatures);
+            vector<double> features;
+            features.push_back(greenFeature);
+            features.insert(features.end(),textureFeatures.begin(),textureFeatures.end());
+            //CALL SVM WITH FEATURES
+            /*
+            //print features
+            for(int i = 0; i< features.size();i++){
+                cout<<features[i]<<" ";
+            }
+            cout<<endl;
+            */
+            window.x = window.x + window.width;
+        }
+        window.x = 0;
+        window.y = window.y + window.height;
+    }
+
+    return 0;
+}
+
+
+/*  EXAMPLE OF USAGE
+
     namedWindow(windowName1);
     namedWindow(windowName2);
     namedWindow(windowName3);
@@ -38,5 +77,4 @@ int main(int argc, char **argv)
     destroyWindow(windowName1);
     destroyWindow(windowName2);
     destroyWindow(windowName3);
-    return 0;
-}
+ */
