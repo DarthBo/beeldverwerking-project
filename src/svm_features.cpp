1d9871f6d603867300b07bc8609c2c9ea02f8c7e
Tibault Damman <tibault.damman@gmail.com>
2014-05-12 22:21:16 +0200

    2 vars met zelfde naam is vragen voor problemen

--- src/svm_features.cpp
+++ src/svm_features.cpp
@@ -13,8 +13,8 @@ void getAverageColour(const cv::Mat& in,std::vector<double>& means){
 void getTextureFeatures(const cv::Mat& in, std::vector<double>& features){
     int orientations[] = {0,45,90,135};
     cv::Mat out;
-    for(int i : orientations){
-        textureFilter(in,out,i);
+    for(int o : orientations){
+        textureFilter(in,out,o);
         std::vector<double> sums;
         squaredSum(out,sums);                   // local energy
         for(size_t i = 0; i< sums.size() ;i++){ // for every channel
