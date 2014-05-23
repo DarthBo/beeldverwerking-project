5223b5e9fe1c02f69e397e62d8efeb7a86ce13ee
Tibault Damman <tibault.damman@gmail.com>
2014-05-23 20:16:40 +0200

    Forgot stderr

--- src/svm_utils.cpp
+++ src/svm_utils.cpp
@@ -607,7 +607,7 @@ bool classify_frame(cv::Mat frame, CharacteristicValue& c, bool skip_datacalc)
     //call svmPerf
     #ifdef _WIN32
         const char* svmperf_class = "svm_perf_classify.exe";
-        const char* nul = ">NUL";
+        const char* nul = ">NUL 2>NUL";
     #else
         const char* svmperf_class = "./svm_perf_classify";
         const char* nul = ">/dev/null 2>/dev/null";
