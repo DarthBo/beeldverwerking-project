137c5f529dde1e3c522db1d49b6061cee8cf7dbc
Tibault Damman <tibault.damman@gmail.com>
2014-05-29 13:52:43 +0200

    Revert "Cleanup for merge", bring back singlethreaded play_classify
    This (partly) reverts commit 1d2ce073c68c516df7f6b6a7607751e2d15d56f3.
    
    Conflicts:
    	src/blindtastic_models.h

--- src/blindtastic_models.cpp
+++ src/blindtastic_models.cpp
@@ -2,7 +2,81 @@
 #include "svm_utils.h"
 #include "blindtastic_concurrency.h"
 
-void play_classify(const char* fvid, int once_every_x_frames, int reset_location_every_x_frames, bool reset_on_skip)
+void play_classify(const char* fvid, int once_every_x_frames, int reset_location_every_x_frames,bool reset_on_skip)
+{
+    const char* winp = "Detecting classifications live...";
+    const char* track = "frame:";
+    struct trackdata data;
+    data.cap.open(fvid);
+    if (!data.cap.isOpened()){
+        return;
+    }
+    featureCallback last;
+    ModelRepository m;
+    LocationRepository locationRepository;
+    int f = 0;
+    cv::namedWindow(winp);
+    cv::createTrackbar(track, winp, &f, getFrameCount(fvid),&trackbar_moved, &data);
+
+    int frames_processed = 1;
+    while(data.cap.isOpened() && data.cap.read(data.img))
+    {
+        ++f;
+
+        if (f % once_every_x_frames == 0)
+        {
+            int match = 0;
+            last = NULL;
+
+            std::map<featureCallback, CharacteristicDefinition>::const_iterator featpair;
+            featpair = m.getCharacteristics().begin();
+
+            while (featpair != m.getCharacteristics().end())
+            {
+                CharacteristicDefinition cdef = featpair->second;
+                double cval;
+                if (featpair->first == last)
+                {
+                    cval = cdef.getValue(data.img, true).weight;
+                }
+                else
+                {
+                    cval = cdef.getValue(data.img, false).weight;
+                    last = featpair->first;
+                }
+
+                if (cval > 0)
+                {
+                    if(frames_processed % reset_location_every_x_frames == 0 || (reset_on_skip && data.skipped > 1)){
+                        locationRepository.resetRefinement();
+                    }
+                    CharacteristicValue cv = cdef.getValue(data.img, true);
+                    locationRepository.refine(cv);
+                    std::string topLocation = locationRepository.getTopLocation().first->getName() +
+                            " : " + std::to_string(locationRepository.getTopLocation().second);
+                    printText(data.img,topLocation, 400,600);
+                    printText(data.img, cdef.getName(), 50, 75 + (35*(match++)));
+                }
+
+                featpair++;
+            }
+            frames_processed++;
+            cv::imshow(winp, data.img);
+            int k = td::waitKey(10);
+            if (k == K_ESC || k == K_Q)
+                data.cap.release();
+        }
+        cv::setTrackbarPos(track, winp, f);
+    }
+
+    if (data.cap.isOpened())
+    {
+        data.cap.release();
+    }
+    cv::destroyWindow(winp);
+}
+
+void play_classify_in_background(const char* fvid, int once_every_x_frames, int reset_location_every_x_frames,bool reset_on_skip)
 {
     const char* winp = "Detecting classifications live...";
     const char* track = "frame:";
--- src/main.cpp
+++ src/main.cpp
@@ -306,7 +306,7 @@ class cCallable : public Callable<std::string>{
 public:
     std::string s;
     std::string call(){return s;}
-    virtual ~cCallable() { }
+    virtual ~cCallable() {}
 };
 
 void testExecutor(){
@@ -352,9 +352,8 @@ int main(int argc, char **argv)
         return 1;
     }
     std::cerr << "Found file at " << videoLocation << "! \nProcessing..." << std::endl;
-
     // do something
-    play_classify(defaultVideo,1);
+    play_classify_in_background(defaultVideo,1);
 
     std::cerr << "Done. Bye!" << std::endl;
     return 0;
